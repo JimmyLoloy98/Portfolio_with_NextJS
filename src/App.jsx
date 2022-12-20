f2f6d9de8d1c54f69b094d2423a988070062996c
Jimmy Loloy <loloy.laurencio@gmail.com>
2022-12-20 16:24:32 -0500

    Initial Setup
    Tech Stack:
    * React
    * TailwindCSS
    * Vite

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { Mail, Github } from "./components/icons.jsx";
+// import Mail, Github from "./components/icons.jsx";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <div className="text-3xl text-slate-600 text-center">{count}</div>
+        <button
+          className="px-6 py-2 flex gap-4 rounded bg-blue-500 hover:bg-blue-700 text-white"
+          type="button"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          <Mail />
+          <Github />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
