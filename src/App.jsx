9bc89e27cd9feeb5f08dee526e15371e3b1e8545
Jimmy Loloy <loloy.laurencio@gmail.com>
2022-12-20 17:31:59 -0500

    feat: navbar added and new font-family

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Mail, Github, Linkedin, Dark, Light } from "./components/icons.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="flex h-screen">
-      <div className="m-auto">
-        <div className="text-3xl text-slate-600 text-center">{count}</div>
-        <button
-          className="px-6 py-2 flex gap-4 rounded bg-blue-500 hover:bg-blue-700 text-white"
-          type="button"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          <Mail />
+      <nav className="w-full px-20 h-20 flex justify-between shadow-md">
+        <a className="flex place-items-center" href="/">
           <Github />
-          <Linkedin />
-          <Dark />
-          <Light />
-        </button>
-      </div>
+        </a>
+
+        <ul className="text-xs font-normal flex place-items-center gap-10 uppercase">
+          {[
+            ["home", "/home"],
+            ["experience", "/experience"],
+            ["achievements", "/achievements"],
+            ["about", "/about"],
+            ["blogs", "/blogs"],
+            ["talks", "/talks"],
+            ["contact", "/contact"],
+          ].map(([title, url]) => (
+            <li>
+              <a href={url} className="hover:text-blue-700 hover:underline">
+                {title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['"Audiowide"', "sans-serif"],
+    },
   },
   plugins: [],
 };
