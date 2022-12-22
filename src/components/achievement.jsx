dd39cce69779a881017ccdebf1610a81387f12fc
Jimmy Loloy <loloy.laurencio@gmail.com>
2022-12-22 15:40:21 -0500

    refactor: center content

--- src/components/achievement.jsx
+++ src/components/achievement.jsx
@@ -27,29 +27,31 @@ export function AchievementSection() {
   return (
     <section
       id="achievements"
-      className="px-4 md:px-4 xl:px-0 grid justify-center my-40 lg:max-w-6xl lg:mx-auto"
+      className="px-4 xl:px-0 flex justify-center my-40 lg:max-w-6xl lg:mx-auto"
     >
-      <h2 className="mb-12 pb-1 text-2xl border-b-2">Achievements</h2>
+      <div className="flex flex-col items-center">
+        <h2 className="mb-12 pb-1 text-2xl border-b-2 w-fit">Achievements</h2>
 
-      <div className="flex flex-col gap-8">
-        {data.map((item) => (
-          <div className="flex items-center gap-4">
-            <figure className="bg-blue-300 rounded-full p-3 md:p-4">
-              {getIcon(item.icon)}
-            </figure>
-            <div className="flex flex-col gap-1">
-              <h3 className="uppercase font-sans2 tracking-wide font-bold text-sm md:text-base lg:text-lg">
-                {item.title}
-              </h3>
-              <p className="text-xs md:text-sm font-semibold text-gray-500 tracking-wider capitalize">
-                {item.level}
-              </p>
-              <p className="text-sm md:text-base font-sans2 font-light text-gray-500 capitalize">
-                {item.date}
-              </p>
+        <div className="flex flex-col gap-8">
+          {data.map((item) => (
+            <div className="flex items-center gap-4">
+              <figure className="bg-blue-300 rounded-full p-3 md:p-4">
+                {getIcon(item.icon)}
+              </figure>
+              <div className="flex flex-col gap-1">
+                <h3 className="uppercase font-sans2 tracking-wide font-bold text-sm md:text-base lg:text-lg">
+                  {item.title}
+                </h3>
+                <p className="text-xs md:text-sm font-semibold text-gray-500 tracking-wider capitalize">
+                  {item.level}
+                </p>
+                <p className="text-sm md:text-base font-sans2 font-light text-gray-500 capitalize">
+                  {item.date}
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </section>
   );
